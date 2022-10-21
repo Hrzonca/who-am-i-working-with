daaed06785b2aa4dc97076562b3112687e15af56
Haleigh Rzonca <hgielah@gmail.com>
2022-10-20 17:14:10 -0700

    added to each of the documents in lib. Should connect all of them to the html page to print out.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-let formatHtml = require('./formatHtml.js');
+let formatHtml = require('./lib/formatHtml.js');
 
 // let markDown = require('./generateMarkDown.js');
 //asks for the mangers information
