b3002b7afd70848b5eafb904096fa897a67b0c89
Haleigh Rzonca <hgielah@gmail.com>
2022-10-24 16:48:47 -0700

    added more testing

--- test/worker.test.js
+++ test/worker.test.js
@@ -1,29 +1,50 @@
 const Worker = require('../lib/worker')
 
-test("can set id via constructor argument", () => {
-    const w = new Worker();
-    expect(typeof(w)).toBe("object")
-})
-
-test("can set id via getName()", () => {
+//testing constructor
+test("can set name via constructor argument", () => {
     const desiredName = 'test'
     const e = new Worker(desiredName)
     expect(e.getName()).toBe(desiredName)
 })
 
-test("can set id via getId()", () => {
+test("can get id via constructor argument", () => {
     const desiredId = 100
     const i = new Worker('test', desiredId)
     expect(i.getId()).toBe(desiredId)
 })
 
-test("can set id via getEmail()", () => {
+test("can get email via constructor argument", () => {
     const desiredEmail = 'test@test.com'
     const e = new Worker('test', 1, desiredEmail)
     expect(e.getEmail()).toBe(desiredEmail)
 })
 
-test("can set role via getRole()", () => {
+test("can get role via constructor argument", () => {
+    const desiredRole = 'worker'
+    const i = new Worker('test', desiredRole)
+    expect(i.getRole()).toBe(desiredRole)
+})
+
+//testing methods
+test("can get id via getName()", () => {
+    const desiredName = 'test'
+    const e = new Worker(desiredName)
+    expect(e.getName()).toBe(desiredName)
+})
+
+test("can get id via getId()", () => {
+    const desiredId = 100
+    const i = new Worker('test', desiredId)
+    expect(i.getId()).toBe(desiredId)
+})
+
+test("can get id via getEmail()", () => {
+    const desiredEmail = 'test@test.com'
+    const e = new Worker('test', 1, desiredEmail)
+    expect(e.getEmail()).toBe(desiredEmail)
+})
+
+test("can get role via getRole()", () => {
     const desiredRole = 'worker'
     const i = new Worker('test', desiredRole)
     expect(i.getRole()).toBe(desiredRole)
